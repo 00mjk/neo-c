1335201bd52a4dd28e55fcf16e1166ed71e5755d
ab25cq <ab25cq@icloud.com>
2020-02-09 01:33:08 +0900

    iaa

--- download_to_client.sh
+++ download_to_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh ab25cq@clover-lang.org bash -c '"cd repo/neo-c; make distclean; cd ..; tar cvfz neo-c.tgz neo-c"'
-scp ab25cq@clover-lang.org:repo/neo-c.tgz ..
-(cd ..; tar xvfz neo-c.tgz)
-cd .
+ssh ab25cq@clover-lang.org bash -c '"rm -f neo-c.tgz; cd repo/neo-c; make distclean; cd ..; tar cvfz ~/neo-c.tgz neo-c"'
+cd ..; rm -rf neo-c
+scp ab25cq@clover-lang.org:neo-c.tgz .
+tar xvfz neo-c.tgz
 
