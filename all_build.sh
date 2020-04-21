f267e405994458ddb6814a76f2131b91eba803ed
ab25cq <ab25cq@icloud.com>
2020-04-21 11:50:03 +0900

    iia

--- all_build.sh
+++ all_build.sh
@@ -1,1 +1,1 @@
-./configure --with-optimize && make && sudo make install && make test && cd wi && ./configure --with-optimize --with-memleak-debug && make && sudo make install && cd ../tiny && ./configure --with-optimize && make && sudo make install && make test && cd ..
+./configure --with-optimize && make && sudo make install && make test && cd wi && ./configure --with-optimize  && make && sudo make install && cd ../tiny && ./configure --with-optimize && make && sudo make install && make test && cd ..
